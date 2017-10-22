15413fb968cf5e50f623b08e37e7ec80c8fe851c
Husain Panatas <husain.panatas@gmail.com>
2017-10-22 22:54:32 +0700

    Update Member_test.php

--- application/tests/controllers/Member_test.php
+++ application/tests/controllers/Member_test.php
@@ -44,5 +44,85 @@ class Admin_test extends TestCase
         $output = $this->request('GET', 'Admin/create');
         $this->assertNull($output);
 	}
+	
+	public function test_save_berhasil(){
+                $_SESSION['username'] = "kurakura";
+                $_SESSION['nama'] = "Husain";
+               
+		$mula = $this->obj->getCurrentRow();
+		$this->request(
+			'POST',
+			['Admin', 'save'],
+			[
+			'nrp' => '5215100131',
+                        'nama_depan' => 'Yuniar Permatasari',
+                        'nama_belakang'    => 'yun@gmailcom',
+                        'kata_sandi'     => '123456'
+			]
+			);
+		$akhir = $this->obj->getCurrentRow();
+        $expected = $akhir - $mula;
+        $this->assertEquals(1, $expected);
+        
+	}
+        
+        public function test_save_tanpa_session(){
+                $mula = $this->obj->getCurrentRow();
+		$this->request(
+			'POST',
+			['Admin', 'save'],
+			[
+			'nrp' => '5215100153',
+                        'nama_depan' => 'Yuniar Permatasari',
+                        'nama_belakang'    => 'yun@gmailcom',
+                        'kata_sandi'     => '123456'
+			]
+			);
+		$akhir = $this->obj->getCurrentRow();
+        $expected = $akhir - $mula;
+        $this->assertEquals(0, $expected);
+        
+	}
+        
+         public function test_save_dikosongi(){
+                $_SESSION['username'] = "kurakura";
+                $_SESSION['nama'] = "Husain";
+              
+		$mula = $this->obj->getCurrentRow();
+		$this->request(
+			'POST',
+			['Admin', 'save'],
+			[
+			'nrp' => '5215100111',
+                        'nama_depan' => 'Yuniar',
+                        'nama_belakang'    => '',
+                        'kata_sandi'     => '123456',
+                        'passconf'     => '123456'
+			]
+			);
+		$akhir = $this->obj->getCurrentRow();
+        $expected = $akhir - $mula;
+        $this->assertEquals(0, $expected);
+        
+	}
+        public function test_save_nrpsudahada(){
+                $_SESSION['username'] = "kurakura";
+                $_SESSION['nama'] = "Husain";
+              
+		$mula = $this->obj->getCurrentRow();
+		$this->request(
+			'POST',
+			['Admin', 'save'],
+			[
+			'nrp' => '5215100011',
+                        'nama_depan' => 'Yuniar',
+                        'nama_belakang'    => 'Permatasari',
+                        'kata_sandi'     => '123456'
+			]
+			);
+		$akhir = $this->obj->getCurrentRow();
+        $expected = $akhir - $mula;
+        $this->assertEquals(0, $expected);  
+	}
     
 }
